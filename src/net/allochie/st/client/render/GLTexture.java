5dd4b9a61311a86430dc3fdf58e5329b10df8488
Lochie <afterlifelochie@afterlifelochie.net>
2015-06-10 22:31:35 +1000

    Fix missing derive(), woops.

--- src/net/allochie/st/client/render/GLTexture.java
+++ src/net/allochie/st/client/render/GLTexture.java
@@ -4,4 +4,10 @@ import net.allochie.st.shared.render.ITexture;
 
 public class GLTexture implements ITexture {
 
+	@Override
+	public ITexture derive(int u0, int v0, int u1, int v1) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
 }
